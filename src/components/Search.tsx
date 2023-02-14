10bc80e3a2e2e8ed5064bf92b9c8221ac97326ef
lookjota <lookjota@gmail.com>
2023-02-14 13:17:46 -0300

    request api

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,16 +2,24 @@ type SearchProps = {
   loadUser: (userName: string) => Promise<void>
 }
 
+import { useState } from "react"
+
 import { BsSearch } from 'react-icons/bs'
 
 const Search = ({loadUser}: SearchProps) => {
+  const [userName, setUserName] = useState("")
+
   return (
     <div>
         <h2>Busque por um usuario</h2>
         <p>Conheca seus melhores repositorios</p>
         <div>
-            <input type="text" placeholder="Digite o nome do usuario" />
-            <button>
+            <input 
+              type="text" 
+              placeholder="Digite o nome do usuario"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <button onClick={() =>loadUser(userName)}>
                 <BsSearch />
             </button>
         </div>    
