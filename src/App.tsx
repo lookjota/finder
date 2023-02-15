9c4788806e697ec81ff950955c5af6ade9ec3b45
lookjota <lookjota@gmail.com>
2023-02-15 12:11:50 -0300

    error using css.module in serach.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom"
 
-import classes from './App.module.css'
+import classess from './App.module.css'
 
 
 function App() {
 
   return (
-    <div className={classes.app}>
+    <div className={classess.app}>
       <h1>Github Finder</h1>
       <Outlet/>
     </div>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,11 +6,13 @@ import { useState } from "react"
 
 import { BsSearch } from 'react-icons/bs'
 
+import classes from "./Search.module.css"
+
 const Search = ({loadUser}: SearchProps) => {
   const [userName, setUserName] = useState("")
 
   return (
-    <div>
+    <div className={classes.search}>
         <h2>Busque por um usuario</h2>
         <p>Conheca seus melhores repositorios</p>
         <div>
